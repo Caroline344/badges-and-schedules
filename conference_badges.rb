3ae1e6ca37b65d786665d100bbd4f7de37bc4a18
Caroline <carolinechavezt@outlook.com>
2021-01-13 21:35:04 -0500

    commit tree 210ab3d5ac157ba4c1a9e3621c6289c6479ca996

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+    names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+    names.each do |name|
+    puts "Hello my name is #{name}."
+    end
+end
